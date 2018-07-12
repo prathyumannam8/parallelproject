d382d919382c7bf9c73b83c05306f20275fe14b4
mprathyu <mprathyu@DIN80000403.corp.capgemini.com>
2018-07-12 09:23:29 +0530

    Version1

---
+++ Plp1/src/com/cg/prathyusha/Prathyusha.java
@@ -0,0 +1,10 @@
+package com.cg.prathyusha;
+
+public class Prathyusha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Prathyusha Mannam");
+	}
+
+}
